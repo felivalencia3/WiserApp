a9a382524bd86fb692f688dcd9ef451fafdfc18d
Felipe Valencia <fvalencia@vc-soft.com>
2021-01-17 16:00:48 -0500

    First version

---
+++ src/main/java/dwight/global/wiser/WiserOnlineApplication.java
@@ -0,0 +1,13 @@
+package dwight.global.wiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiserOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiserOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dwight/global/wiser/WiserOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package dwight.global.wiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiserOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
