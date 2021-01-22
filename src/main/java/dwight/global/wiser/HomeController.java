e05787afe9633f034b2f758fe7c76bad9479b527
Felipe Valencia <fvalencia@vc-soft.com>
2021-01-22 16:02:32 -0500

    Implemented Mustache engine

--- src/main/java/dwight/global/wiser/HomeController.java
+++ src/main/java/dwight/global/wiser/HomeController.java
@@ -2,10 +2,14 @@ package dwight.global.wiser;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -13,26 +17,19 @@ public class HomeController {
     public SubmissionRepository submissionRepository;
 
     @GetMapping("/")
-    public @ResponseBody Iterable<Submission> home() {
-        return submissionRepository.findAll();
+    public String home(@RequestParam(name="name", defaultValue = "name", required = false) String name, Map<String, Object> model) {
+        model.put("name", name);
+        return "home";
+        //return submissionRepository.findAll();
     }
 
     @PostMapping("/admin/add")
-    public @ResponseBody String addNewSubmission(@RequestParam String artist, @RequestParam String title) {
+    public @ResponseBody
+    String addNewSubmission(@RequestParam String artist, @RequestParam String title) {
         Submission n = new Submission();
         n.setArtist(artist);
         n.setTitle(title);
         submissionRepository.save(n);
         return "Saved";
-    }
-
-    @GetMapping("/about")
-    public String about() {
-        return "about";
-    }
-
-    @GetMapping("/admin")
-    public String admin() {
-        return "admin";
     }
 }
