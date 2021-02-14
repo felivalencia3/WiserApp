a760fcedaa0c0db8c6f5bc61482d5266c69d6d0d
Felipe Valencia <fvalencia@vc-soft.com>
2021-02-14 13:48:20 -0500

    Implemented Swagger UI

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+export SPRING_JPA_HIBERNATE_DDL_AUTO=update
+export SPRING_DATASOURCE_URL=jdbc:mysql://localhost:3306/wiser
+export SPRING_DATASOURCE_USERNAME=root
+export SPRING_DATASOURCE_PASSWORD=castro03
+export SPRING_JPA_DATABASE_PLATFORM=org.hibernate.dialect.MySQL5Dialect
+
+
+mvn clean package
